f71e106719a83630c67b3a3267bf57e30c4f0be6
thisisasecretname <cyril@spacefox.shop>
2022-05-11 16:58:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# phaser-pwa
+
+Phaser PWA
+
+This is based on a tutorial for making a Phaser PWA:
+
+> Phaser Progressive Web Apps Tutorial – How to Create Offline-First Games \
+> https://gamedevacademy.org/phaser-progressive-web-apps-tutorial/
+
+# Run it online
+
+https://koden-km.github.io/phaser-pwa
+
+# Local setup for dev
+
+Clone repo and run it locally.
+
+## Install
+
+```
+$ yarn install
+```
+
+## Running local server
+
+```
+$ yarn run server
+```
---
+++ sw.js
@@ -0,0 +1,51 @@
+var cacheName = 'phaser-v1';
+var filesToCache = [
+    // '/',
+    '/phaser-pwa-master/',
+    'index.html',
+    'img/logo.png',
+    'img/icon-192.png',
+    'img/icon-256.png',
+    'img/icon-512.png',
+    'js/game.js',
+    'css/style.css',
+    'https://cdn.jsdelivr.net/gh/photonstorm/phaser@3.55.2/dist/phaser.min.js'
+];
+
+self.addEventListener('install', function(event) {
+    console.log('sw install');
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('sw caching files');
+            return cache.addAll(filesToCache);
+        }).catch(function(err) {
+            console.log(err);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('sw fetch');
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        }).catch(function(error) {
+            console.log(error);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('sw activate');
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('sw removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
